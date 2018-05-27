57f55396c13c3f05c8d6f663cb733864c7b4a446
Yusuf Akdas <yusufakdas@gtu.edu.tr>
2018-05-27 08:20:34 +0300

    Added library feature to game
    TODO: Description and last searched items sections

---
+++ ProceduralLandmassGeneration/Assets/Scripts/ExploreLibraryScripts/EntityInfo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EntityInfo : MonoBehaviour
+{
+    public Button livingThingButton;
+    public Text theNameOfThing;
+
+    private LivingThing thing;
+    private EntityScrollList list;
+
+	public void Setup (LivingThing currentItem, EntityScrollList entityList)
+    {
+        thing = currentItem;
+        list = entityList;
+        theNameOfThing.text = thing.getName();
+	}
+}
---
+++ ProceduralLandmassGeneration/Assets/Scripts/ExploreLibraryScripts/EntityScrollList.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EntityScrollList : MonoBehaviour 
+{
+    private List<LivingThing> itemList = World.getExplored();
+    public Transform contentPanel;
+    public ObjectPoolScript entityObject;
+    private InputField searchField;
+    private List<KeyValuePair<string, GameObject>> pair = new List<KeyValuePair<string, GameObject>>();
+
+    void Start ()
+    {
+        searchField = GameObject.Find("SearchField").GetComponent<InputField>();
+        searchField.onValueChanged.AddListener( delegate { CheckVisibility(); });
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        HeapSort.Sort(itemList);
+        AddEntities();
+    }
+
+    private void AddEntities()
+    {
+        pair.Clear();
+        for (int i = 0; i < itemList.Count; ++i)
+        {
+            LivingThing currentItem = itemList[i];
+            GameObject newItem = entityObject.GetObject();
+            newItem.transform.SetParent(contentPanel);
+            EntityInfo element = newItem.GetComponent<EntityInfo>();
+            element.Setup(currentItem, this);
+            pair.Add(new KeyValuePair<string, GameObject>(currentItem.getName(), newItem));
+        }
+    }
+
+    public void CheckVisibility()
+    {
+        string value = searchField.text;
+        for (int i = 0; i < pair.Count; ++i)
+        {
+            if (value.Length <= pair[i].Key.Length)
+            {
+                if (value.CompareTo(pair[i].Key.Substring(0, value.Length)) != 0)
+                    pair[i].Value.SetActive(false);
+                else
+                    pair[i].Value.SetActive(true);
+            }
+            else
+            {
+                pair[i].Value.SetActive(false);
+            }
+        }
+    }
+}
+
